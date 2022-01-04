405cbf7d6b42b51f74c300778a9fd00fbf421a34
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-04 19:22:25 +0530

    cleanup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import {useState , useEffect} from 'react'
+import axios from 'axios'
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className="title has-text-centered">This  is client page</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/Login.js
@@ -0,0 +1,11 @@
+
+
+function Login() {
+    return (
+        <div>
+            <h1>sad</h1>
+        </div>
+    )
+}
+
+export default Login
