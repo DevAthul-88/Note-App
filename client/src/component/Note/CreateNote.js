b8cb49b49c5afe48e271b7add7eafa23b4b53e96
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-06 18:54:18 +0530

    create note

--- client/src/component/Note/CreateNote.js
+++ client/src/component/Note/CreateNote.js
@@ -1,6 +1,13 @@
 import React,{useState} from "react";
+import axios from "axios";
+import {
+    Redirect
+  } from "@reach/router"
 
 function CreateNote() {
+
+ 
+
 
     const [note , setNote] = useState({
         title:"",
@@ -8,19 +15,60 @@ function CreateNote() {
         date:''
     }) 
 
+    function onChangeValues(e){
+      const {name , value} = e.target
+      setNote((prev) => {
+          return{
+              ...prev,
+              [name]: value
+          }
+      })
+    }
+
+
+   async function onSubmit(e){
+       e.preventDefault()
+
+       const token = localStorage.getItem("tokenStore")
+       
+       if(token){
+           const {title , description , date} = note
+
+           const newNote = {title , description , date}
+ 
+           await axios.post('/note' , newNote , {
+                headers: { Authorization: token }
+           })
+
+           return window.location.href = '/'
+       }
+
+       try {
+           
+       } catch (error) {
+           window.location.href = '/'
+       }
+    }
 
 
   return (
     <div>
       <h1 className="title">Create Note</h1>
 
-      <label className="label">Note Title</label>
-      <input type="text" value={note.title} name="title" className="input" required/>
+     <form onSubmit={onSubmit}>
+
+     <label className="label">Note Title</label>
+      <input type="text" value={note.title} onChange={onChangeValues} name="title" className="input" required/>
 
       <label className="label mt-4">Note Description</label>
-      <textarea name="description" value={note.description} className="textarea" required></textarea>
+      <textarea name="description" value={note.description} onChange={onChangeValues} className="textarea" required></textarea>
 
-      <button className="mt-4 button is-primary">Submit</button>
+     
+
+       <label className="label mt-4">Date: {note.date}</label>
+       <input type="date" name="date" className="input" onChange={onChangeValues} required/>
+       <button type='submit' className="mt-4 button is-primary">Submit</button>
+     </form>
     </div>
   );
 }
