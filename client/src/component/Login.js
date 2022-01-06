765445ced989604073741dd76b4f4de1de07182e
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-01-06 17:45:41 +0530

    bug fix

--- client/src/component/Login.js
+++ client/src/component/Login.js
@@ -1,81 +1,86 @@
-import {Link} from '@reach/router'
-import {useState} from 'react'
-import axios from 'axios'
+import { Link } from "@reach/router";
+import { useState } from "react";
+import axios from "axios";
 
-function Login({setIsLogged}) {
+function Login({ setIsLogged }) {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [user , setUser] = useState({
-    
-        email:'',
-        password:'',
-    })
+  const [error, setError] = useState(null);
 
-    const [error, setError] = useState(null)
+  function onChange(e) {
+    const { name, value } = e.target;
 
-    function onChange(e){
+    setUser((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
-        const {name , value} = e.target
-
-        setUser((prev) => {
-            return{
-                ...prev,
-                [name]:value
-            }
-        })
-
+  async function onSubmit(e) {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/user/login", user);
+         
+      localStorage.setItem("tokenStore", res.data.token);
+      setIsLogged(true);
+    } catch (error) {
+      error.response.data.error && setError(error.response.data.error);
     }
+  }
 
-   async function onSubmit(){
-        try {
-            const res = await axios.post('/user/login' , user)
-            
-
-            localStorage.setItem('tokenStore'  , res.data.token)
-            setIsLogged(true)
-        } catch (error) {
-            error.response.data.error && setError(error.response.data.error)
-        }
-    }
-
-
-    return (
-        <div className="section">
-
-            
-          {
-              error !== null ? (
-               <div>
-
-                    <article className="message is-danger">
-                <div className="message-body">
-                 {error}
-                </div>
-              </article>
-
-               </div>
-              ) : (null)
-          }
-
-
-            <h1 className="title is-size-1 has-text-weight-bold has-text-centered">Login</h1>
-       
-        
-         <label htmlFor="" className="label mt-4">Email Address</label>
-         <input type="email"  className="input" name='email' value={user.email} onChange={onChange}  required/>
-
-         <label htmlFor="" className="label mt-4">Password</label>
-         <input type="password"  className="input" name='password' value={user.password} onChange={onChange}  required/>
-
-          
-          
-      
-
-         <button onClick={onSubmit} className='mt-4 button is-primary'>
-             <strong>Submit</strong>
-         </button>
-
+  return (
+    <div className="section">
+      {error !== null ? (
+        <div>
+          <article className="message is-danger">
+            <div className="message-body">{error}</div>
+          </article>
         </div>
-    )
+      ) : null}
+
+      <h1 className="title is-size-1 has-text-weight-bold has-text-centered">
+        Login
+      </h1>
+
+      <form onSubmit={onSubmit}>
+      <label htmlFor="" className="label mt-4">
+        Email Address
+      </label>
+      <input
+         required
+        type="email"
+        className="input"
+        name="email"
+        value={user.email}
+        onChange={onChange}
+       
+      />
+
+      <label htmlFor="" className="label mt-4">
+        Password
+      </label>
+      <input
+        required
+        type="password"
+        className="input"
+        name="password"
+        value={user.password}
+        onChange={onChange}
+       
+      />
+
+      <button type='submit' className="mt-4 button is-primary">
+        <strong>Submit</strong>
+      </button>
+
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/component/Note/CreateNote.js
+++ client/src/component/Note/CreateNote.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 
 function CreateNote() {
 
-    
+    const [note , setNote] = useState({
+        title:"",
+        description:"",
+        date:''
+    }) 
 
 
 
@@ -11,10 +15,10 @@ function CreateNote() {
       <h1 className="title">Create Note</h1>
 
       <label className="label">Note Title</label>
-      <input type="text" name="title" className="input" />
+      <input type="text" value={note.title} name="title" className="input" required/>
 
       <label className="label mt-4">Note Description</label>
-      <textarea name="description" id="" className="textarea"></textarea>
+      <textarea name="description" value={note.description} className="textarea" required></textarea>
 
       <button className="mt-4 button is-primary">Submit</button>
     </div>
